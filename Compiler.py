fd8f7edac905b2f6527edc61b99eff6131ce39b3
Shaheer Mustafa Awan <shaheermustafaawan@gmail.com>
2025-12-19 18:03:23 +0500

    Parser done

--- Compiler.py
+++ Compiler.py
@@ -76,3 +76,79 @@ def lex(text: str) -> List[Token]:
 
     tokens.append(Token("EOF", "", i))
     return tokens
+
+# ------------------------------------
+# AST NODES
+# ------------------------------------
+@dataclass
+class ASTNode:
+    pass
+
+@dataclass
+class Program(ASTNode):
+    statements: List['Stmt']
+
+@dataclass
+class Stmt(ASTNode):
+    kind: str            # MOVE, TURN, JUMP, ATTACK
+    arg: Optional[int]   # number for MOVE,JUMP
+    subkind: Optional[str]  # LEFT/RIGHT for TURN
+    pos: int
+
+# ------------------------------------
+# PARSER 
+# ------------------------------------
+class Parser:
+    def __init__(self, tokens: List[Token]):
+        self.tokens = tokens
+        self.i = 0
+
+    def peek(self):
+        return self.tokens[self.i]
+
+    def consume(self, expected_type=None):
+        tok = self.peek()
+        if expected_type and tok.type != expected_type:
+            raise SyntaxError(
+                f"Expected {expected_type} at pos {tok.pos}, got {tok.type}"
+            )
+        self.i += 1
+        return tok
+
+    def parse(self) -> Program:
+        stmts = []
+        while self.peek().type != "EOF":
+            stmts.append(self.parse_stmt())
+        return Program(stmts)
+
+    def parse_stmt(self) -> Stmt:
+        tok = self.peek()
+
+        if tok.type == "MOVE":
+            self.consume("MOVE")
+            num = self.consume("NUMBER")
+            self.consume("SEMICOLON")
+            return Stmt("MOVE", int(num.value), None, tok.pos)
+
+        elif tok.type == "TURN":
+            self.consume("TURN")
+            d = self.peek()
+            if d.type not in ("LEFT", "RIGHT"):
+                raise SyntaxError(f"Expected LEFT or RIGHT at pos {d.pos}")
+            direction = self.consume().type
+            self.consume("SEMICOLON")
+            return Stmt("TURN", None, direction, tok.pos)
+
+        elif tok.type == "JUMP":
+            self.consume("JUMP")
+            num = self.consume("NUMBER")
+            self.consume("SEMICOLON")
+            return Stmt("JUMP", int(num.value), None, tok.pos)
+
+        elif tok.type == "ATTACK":
+            self.consume("ATTACK")
+            self.consume("SEMICOLON")
+            return Stmt("ATTACK", None, None, tok.pos)
+
+        else:
+            raise SyntaxError(f"Unexpected token {tok.type} at pos {tok.pos}")
